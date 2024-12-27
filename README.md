84ba33b93b8f5e0ec5f4af780356d0816024b4ea
TejasviHarshadNetra <tejasvi.rananaware@gmail.com>
2024-12-27 11:35:56 +0530

    Added latest files to this branch

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# mAttendanceLatest
-LatestCodemAttendance
+# mAttendanceAPI
---
+++ ServiceContainer.cs
@@ -0,0 +1,46 @@
+﻿using mAttendanceAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace mAttendanceAPI
+{
+    public static class ServiceContainer
+    {
+        public static void RegisterDependancies(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.WithOrigins("*")
+                                      .AllowAnyHeader()
+                                      .AllowAnyMethod());
+            });
+            services.AddDbContext<ApplicationDBContext>(option =>
+            {
+                string connectionstring = configuration["ConnectionStrings:DBConnection"]!;
+                option.UseSqlServer(connectionstring);
+            });
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidAudience = configuration["JWT:Audiance"],
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]!))
+                };
+
+            });
+        }
+
+    }
+}
